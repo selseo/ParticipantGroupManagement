9421a4106128d4be27f540c3c2bdf093c6ed3f23
Katesuda.l <Katesuda.l@hotmail.com>
2016-11-29 12:49:45 +0700

    Create UI for create project

--- app/src/main/java/com/example/katesudal/participantgroupmanagement/CreateProjectActivity.java
+++ app/src/main/java/com/example/katesudal/participantgroupmanagement/CreateProjectActivity.java
@@ -1,13 +1,94 @@
 package com.example.katesudal.participantgroupmanagement;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ClipData;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.DragEvent;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class CreateProjectActivity extends AppCompatActivity {
+    private ViewGroup layoutUnselectedParticipantName;
+    private Realm realm;
+    private int number = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        realm = Realm.getDefaultInstance();
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         setContentView(R.layout.activity_create_project);
+        layoutUnselectedParticipantName = (ViewGroup) findViewById(R.id.layoutUnselectedParticipantName);
+        createParticipantNameItem(realm,layoutUnselectedParticipantName,inflater);
+    }
+
+    private void createParticipantNameItem(Realm realm, ViewGroup itemLayout, LayoutInflater inflater) {
+        RealmResults<Participant> participants = realm.where(Participant.class).findAll();
+        for (int participantIndex = 0; participantIndex < participants.size(); participantIndex++) {
+            View itemView = inflater.inflate(R.layout.item_participant_name, null);
+            TextView itemName = (TextView) itemView.findViewById(R.id.textViewItemParticipantName);
+            itemName.setText(participants.get(participantIndex).getParticipantName());
+            itemLayout.addView(itemView);
+            itemView.setOnTouchListener(new OnTouchItem());
+        }
+    }
+
+
+    class OnTouchItem implements View.OnTouchListener {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent arg1) {
+            ClipData data = ClipData.newPlainText("", "");
+            View.DragShadowBuilder shadow = new View.DragShadowBuilder(v);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                v.startDragAndDrop(data, shadow, v, 0);
+            } else {
+                v.startDrag(data, shadow, v, 0);
+            }
+            v.setVisibility(View.INVISIBLE);
+            return true;
+        }
+    }
+
+    class OnDragItem implements View.OnDragListener {
+
+        @Override
+        public boolean onDrag(View view, DragEvent dragEvent) {
+            final int action = dragEvent.getAction();
+            switch (action) {
+
+                case DragEvent.ACTION_DRAG_STARTED:
+                    break;
+
+                case DragEvent.ACTION_DRAG_EXITED:
+                    break;
+
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    break;
+
+                case DragEvent.ACTION_DROP: {
+                    View viewState = (View) dragEvent.getLocalState();
+                    ViewGroup viewgroup = (ViewGroup) viewState.getParent();
+                    viewgroup.removeView(viewState);
+                    ViewGroup containView = (ViewGroup) view;
+                    containView.addView(viewState);
+                    viewState.setVisibility(View.VISIBLE);
+                    break;
+                }
+                case DragEvent.ACTION_DRAG_ENDED: {
+                    return (true);
+                }
+                default:
+                    break;
+            }
+            return true;
+        }
     }
 }
--- app/src/main/java/com/example/katesudal/participantgroupmanagement/MainActivity.java
+++ app/src/main/java/com/example/katesudal/participantgroupmanagement/MainActivity.java
@@ -3,17 +3,24 @@ package com.example.katesudal.participantgroupmanagement;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button buttonEditParticipant;
+    private Button buttonCreateProject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Realm.init(getApplicationContext());
         setContentView(R.layout.activity_main);
+        buttonCreateProject = (Button) findViewById(R.id.buttonCreateProject);
+        buttonCreateProject.setOnClickListener(this);
         buttonEditParticipant = (Button) findViewById(R.id.buttonEditParticipant);
         buttonEditParticipant.setOnClickListener(this);
     }
@@ -21,7 +28,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         if(view.getId()==R.id.buttonEditParticipant){
-            Intent intent = new Intent(view.getContext(), AddParticipantActivity.class);
+            Intent intent = new Intent(view.getContext(), EditParticipantActivity.class);
+            startActivity(intent);
+        }
+        if(view.getId()==R.id.buttonCreateProject){
+            Intent intent = new Intent(view.getContext(), CreateProjectActivity.class);
             startActivity(intent);
         }
     }
