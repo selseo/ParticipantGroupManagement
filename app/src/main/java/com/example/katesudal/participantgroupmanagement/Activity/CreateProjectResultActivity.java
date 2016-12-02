8a11dccc9221390d980258b463b7f9d0cf4a4aea
Katesuda.l <Katesuda.l@hotmail.com>
2016-12-02 16:58:31 +0700

    Add back to main button from create project result

--- app/src/main/java/com/example/katesudal/participantgroupmanagement/Activity/CreateProjectResultActivity.java
+++ app/src/main/java/com/example/katesudal/participantgroupmanagement/Activity/CreateProjectResultActivity.java
@@ -1,8 +1,11 @@
 package com.example.katesudal.participantgroupmanagement.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.katesudal.participantgroupmanagement.Model.Participant;
@@ -15,7 +18,9 @@ import java.util.List;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
-public class CreateProjectResultActivity extends AppCompatActivity {
+public class CreateProjectResultActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button buttonBacktoMainFromCreateProject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,8 @@ public class CreateProjectResultActivity extends AppCompatActivity {
         realm = Realm.getDefaultInstance();
         setContentView(R.layout.activity_create_project_result);
         TextView textViewResult = (TextView) findViewById(R.id.textViewResult);
+        buttonBacktoMainFromCreateProject = (Button) findViewById(R.id.buttonBacktoMainFromCreateProject);
+        buttonBacktoMainFromCreateProject.setOnClickListener(this);
         String result ="";
         RealmResults<Project> projects = realm.where(Project.class).findAll();
         for(int projectIndex = 0; projectIndex < projects.size(); projectIndex++){
@@ -38,5 +45,13 @@ public class CreateProjectResultActivity extends AppCompatActivity {
         }
         Log.d("Result",result);
         textViewResult.setText(result);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.buttonBacktoMainFromCreateProject){
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
