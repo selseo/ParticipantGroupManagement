8a65a428ea5fcfead4d86cb54406cc5c60f3cb0b
Katesuda.l <Katesuda.l@hotmail.com>
2016-12-08 11:16:50 +0700

    Add duplicateSectionName test

---
+++ app/src/test/java/ValidateUtilTest.java
@@ -0,0 +1,22 @@
+import com.example.katesudal.participantgroupmanagement.Util.ValidateUtil;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by katesuda.l on 08/12/2559.
+ */
+
+public class ValidateUtilTest {
+
+    @Test
+    public void duplicateSectionName()throws Exception{
+        List<String> names = new ArrayList<>();
+        names.add("Thai");
+
+        Assert.assertTrue(ValidateUtil.isDuplicateSectionName("Thai",names));
+    }
+}
