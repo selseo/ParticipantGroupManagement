7729ffbadd61e4b634c2f86a65c9b5b2509e607a
Katesuda.l <Katesuda.l@hotmail.com>
2016-12-09 14:55:50 +0700

    Add showUncheckedGenderDialog UI test

--- app/src/androidTest/java/com/example/katesudal/participantgroupmanagement/AddParticipantTest.java
+++ app/src/androidTest/java/com/example/katesudal/participantgroupmanagement/AddParticipantTest.java
@@ -14,8 +14,10 @@ import org.junit.Test;
 
 import io.realm.Realm;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
@@ -49,6 +51,20 @@ public class AddParticipantTest {
         onView(withId(R.id.radioParticipant)).perform(click());
         onView(withId(R.id.buttonAddParticipant)).perform(click());
         onView(withText("Participant Name should begin with letter and contain only letter or number. \nPlease try another.")).check(matches(isDisplayed()));
+        onView(allOf(
+                instanceOf(Button.class),
+                withText("OK")))
+                .perform(click());
+        onView(withId(R.id.buttonAddParticipant)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void showUncheckedGenderDialog() throws Exception{
+        onView(withId(R.id.editTextParticipantName)).perform(typeText("Anna"));
+        closeSoftKeyboard();
+        onView(withId(R.id.radioParticipant)).perform(click());
+        onView(withId(R.id.buttonAddParticipant)).perform(click());
+        onView(withText("Gender or Type should be selected.")).check(matches(isDisplayed()));
         onView(allOf(
                 instanceOf(Button.class),
                 withText("OK")))
