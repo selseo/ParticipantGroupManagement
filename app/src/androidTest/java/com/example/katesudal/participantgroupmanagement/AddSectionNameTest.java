1375a376c27043010cb13e812e8545e73e7a075b
Katesuda.l <Katesuda.l@hotmail.com>
2016-12-13 18:39:57 +0700

    Add addDuplicateSectionName test

--- app/src/androidTest/java/com/example/katesudal/participantgroupmanagement/AddSectionNameTest.java
+++ app/src/androidTest/java/com/example/katesudal/participantgroupmanagement/AddSectionNameTest.java
@@ -17,6 +17,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Created by katesuda.l on 13/12/2559.
@@ -37,6 +38,20 @@ public class AddSectionNameTest {
                 withText("OK")))
                 .perform(click());
         onView(withId(R.id.buttonCancelCreateSectionName)).perform(click());
+    }
+
+    @Test
+    public void addDuplicateSectionName() throws Exception{
+        onView(withId(R.id.editTextSectionName)).perform(typeText("Thai"));
+        onView(withId(R.id.buttonAddSectionName)).perform(click());
+        onView(withId(R.id.editTextSectionName)).perform(typeText("Thai"));
+        onView(withId(R.id.buttonAddSectionName)).perform(click());
+        onView(withText("This Section Name is Duplicate. \nPlease try another.")).check(matches(isDisplayed()));
+        onView(allOf(
+                instanceOf(Button.class),
+                withText("OK")))
+                .perform(click());
+        onView(withId(R.id.buttonCancelCreateSectionName)).check(matches(isDisplayed()));
     }
 
 
