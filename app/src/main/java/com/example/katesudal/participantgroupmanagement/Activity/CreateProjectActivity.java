cd09caf07acbc9dda37ff15f00f06ad09f4730f1
Katesuda.l <Katesuda.l@hotmail.com>
2016-12-13 13:45:26 +0700

    Refactor and use butterknife in createProject

--- app/src/main/java/com/example/katesudal/participantgroupmanagement/Activity/CreateProjectActivity.java
+++ app/src/main/java/com/example/katesudal/participantgroupmanagement/Activity/CreateProjectActivity.java
@@ -1,12 +1,10 @@
 package com.example.katesudal.participantgroupmanagement.Activity;
 
-import android.app.Dialog;
 import android.content.ClipData;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -25,46 +23,62 @@ import com.example.katesudal.participantgroupmanagement.Model.Project;
 import com.example.katesudal.participantgroupmanagement.PreferencesService;
 import com.example.katesudal.participantgroupmanagement.R;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.realm.Realm;
 import io.realm.RealmList;
 import io.realm.RealmResults;
 
+
 public class CreateProjectActivity extends AppCompatActivity implements View.OnClickListener{
-    private ViewGroup layoutUnselectedParticipantName;
-    private ViewGroup layoutGroupSelectedParticipants;
-    private LinearLayout layoutGroups;
-    private LinearLayout buttonSubmitCreateProject;
-    private LinearLayout buttonCancelCreateProject;
-    private LinearLayout layoutCreateProject;
-    private ScrollView scrollViewUnselected;
-    private ScrollView scrollViewSelected;
-    private TextView textViewCreatedProjectName;
+
+    @BindView(R.id.layoutUnselectedParticipantName)
+    ViewGroup layoutUnselectedParticipantName;
+    @BindView(R.id.layoutGroups)
+    LinearLayout layoutGroups;
+    @BindView(R.id.buttonSubmitCreateProject)
+    LinearLayout buttonSubmitCreateProject;
+    @BindView(R.id.buttonCancelCreateProject)
+    LinearLayout buttonCancelCreateProject;
+    @BindView(R.id.layoutCreateProject)
+    LinearLayout layoutCreateProject;
+    @BindView(R.id.scrollViewUnselected)
+    ScrollView scrollViewUnselected;
+    @BindView(R.id.scrollViewSelected)
+    ScrollView scrollViewSelected;
+    @BindView(R.id.textViewCreatedProjectName)
+    TextView textViewCreatedProjectName;
+
     private Project project;
     private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        realm = Realm.getDefaultInstance();
-        project = (Project) PreferencesService.getPreferences("Project",Project.class,this);
-        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         setContentView(R.layout.activity_create_project);
-        textViewCreatedProjectName = (TextView) findViewById(R.id.textViewCreatedProjectName);
-        layoutUnselectedParticipantName = (ViewGroup) findViewById(R.id.layoutUnselectedParticipantName);
-        layoutGroups = (LinearLayout) findViewById(R.id.layoutGroups);
-        buttonSubmitCreateProject = (LinearLayout) findViewById(R.id.buttonSubmitCreateProject);
-        buttonCancelCreateProject = (LinearLayout) findViewById(R.id.buttonCancelCreateProject);
-        scrollViewUnselected = (ScrollView) findViewById(R.id.scrollViewUnselected);
-        scrollViewSelected = (ScrollView) findViewById(R.id.scrollViewSelected);
-        layoutCreateProject = (LinearLayout) findViewById(R.id.layoutCreateProject);
-        layoutCreateProject.setOnDragListener(new OnDragItem());
+        ButterKnife.bind(this);
+
+        Realm.init(getApplicationContext());
+        realm = Realm.getDefaultInstance();
+
+        project = (Project) PreferencesService.getPreferences("Project",Project.class,this);
         textViewCreatedProjectName.setText("Project Name : "+project.getProjectName());
+
+        setListener();
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        createParticipantNameItem(realm,layoutUnselectedParticipantName,inflater);
+        createSectionLayout(project,inflater,layoutGroups);
+    }
+
+    private void setListener() {
+        layoutCreateProject.setOnDragListener(new OnDragItem());
         buttonSubmitCreateProject.setOnDragListener(new OnDragItem());
         buttonCancelCreateProject.setOnDragListener(new OnDragItem());
         scrollViewUnselected.setOnDragListener(new OnDragItem());
         scrollViewSelected.setOnDragListener(new OnDragItem());
-        createParticipantNameItem(realm,layoutUnselectedParticipantName,inflater);
-        createSectionLayout(project,inflater,layoutGroups);
         buttonSubmitCreateProject.setOnClickListener(this);
         buttonCancelCreateProject.setOnClickListener(this);
     }
@@ -74,42 +88,50 @@ public class CreateProjectActivity extends AppCompatActivity implements View.OnC
         for (int participantIndex = 0; participantIndex < participants.size(); participantIndex++) {
             Participant participant = participants.get(participantIndex);
             View itemView = inflater.inflate(R.layout.item_participant_name, null);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                if(participant.getParticipantType().equals("Staff")){
-                    itemView.setBackground(getDrawable(R.drawable.background_participant_staff_name_item));
-                }
-                else{
-                    itemView.setBackground(getDrawable(R.drawable.background_participant_name_item));
-                }
-
-            } else {
-                if(participant.getParticipantType().equals("Staff")){
-                    itemView.setBackgroundResource(R.drawable.background_participant_staff_name_item);
-                }
-                else{
-                    itemView.setBackgroundResource(R.drawable.background_participant_name_item);
-                }
-            }
             TextView itemName = (TextView) itemView.findViewById(R.id.textViewItemParticipantName);
             itemName.setText(participant.getParticipantName());
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if(participant.getParticipantGender().equals("Male")){
-                    itemName.setTextColor(getColor(R.color.colorVeryLightCream));
-                }
-                else{
-                    itemName.setTextColor(getColor(R.color.colorNormalWhite));
-                }
-            }
-            else{
-                if(participant.getParticipantGender().equals("Male")){
-                    itemName.setTextColor(ContextCompat.getColor(this,R.color.colorVeryLightCream));
-                }
-                else{
-                    itemName.setTextColor(ContextCompat.getColor(this,R.color.colorNormalWhite));
-                }
-            }
+            setItemNameBackground(participant, itemView);
+            setItemNameTextColor(participant, itemName);
             itemLayout.addView(itemView);
             itemView.setOnTouchListener(new OnTouchItem());
+        }
+    }
+
+    private void setItemNameTextColor(Participant participant, TextView itemName) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(participant.getParticipantGender().equals("Male")){
+                itemName.setTextColor(getColor(R.color.colorVeryLightCream));
+            }
+            else{
+                itemName.setTextColor(getColor(R.color.colorNormalWhite));
+            }
+        }
+        else{
+            if(participant.getParticipantGender().equals("Male")){
+                itemName.setTextColor(ContextCompat.getColor(this,R.color.colorVeryLightCream));
+            }
+            else{
+                itemName.setTextColor(ContextCompat.getColor(this,R.color.colorNormalWhite));
+            }
+        }
+    }
+
+    private void setItemNameBackground(Participant participant, View itemView) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if(participant.getParticipantType().equals("Staff")){
+                itemView.setBackground(getDrawable(R.drawable.background_participant_staff_name_item));
+            }
+            else{
+                itemView.setBackground(getDrawable(R.drawable.background_participant_name_item));
+            }
+
+        } else {
+            if(participant.getParticipantType().equals("Staff")){
+                itemView.setBackgroundResource(R.drawable.background_participant_staff_name_item);
+            }
+            else{
+                itemView.setBackgroundResource(R.drawable.background_participant_name_item);
+            }
         }
     }
 
@@ -118,7 +140,7 @@ public class CreateProjectActivity extends AppCompatActivity implements View.OnC
             View layoutView = inflater.inflate(R.layout.layout_group, null);
             TextView textViewSectionName = (TextView) layoutView.findViewById(R.id.textViewGroupName);
             LinearLayout layoutGroupName = (LinearLayout) layoutView.findViewById(R.id.layoutGroupName);
-            layoutGroupSelectedParticipants = (FlowLayout) layoutView.findViewById(R.id.layoutGroupSelectedParticipants);
+            ViewGroup layoutGroupSelectedParticipants = (FlowLayout) layoutView.findViewById(R.id.layoutGroupSelectedParticipants);
             String sectionName = project.getSectionIDs().get(sectionIndex).getSectionName();
             textViewSectionName.setText(sectionName);
             layout.addView(layoutView);
@@ -192,50 +214,63 @@ public class CreateProjectActivity extends AppCompatActivity implements View.OnC
     @Override
     public void onClick(View view) {
         if(view.getId()==R.id.buttonSubmitCreateProject){
-            submitNewProject(view);
+            createNewProject(view);
         }
         if(view.getId()==R.id.buttonCancelCreateProject){
             onBackPressed();
         }
     }
 
-    private void submitNewProject(View view) {
+    private void createNewProject(View view) {
         if(layoutUnselectedParticipantName.getChildCount()>0){
-            AlertDialog.Builder dialogErrorBuilder = new AlertDialog.Builder(this);
-            dialogErrorBuilder.setMessage("Please assign participant to any section");
-            dialogErrorBuilder.setCancelable(false);
-            dialogErrorBuilder.setPositiveButton("OK",new DialogInterface.OnClickListener(){
-                    public void onClick(DialogInterface dialog,int which){
-                        dialog.dismiss();
-                    }
-            });
-            dialogErrorBuilder.show();
+            showDialogNotAssignAllParticipant();
             return;
         }
         project.setProjectID((int) generateProjectID(realm));
+        setSectionInProject();
+        realm.beginTransaction();
+        realm.copyToRealm(project);
+        realm.commitTransaction();
+
+        Intent intent = new Intent(view.getContext(), CreateProjectResultActivity.class);
+        intent.putExtra("projectID",project.getProjectID());
+        startActivity(intent);
+    }
+
+    private void setSectionInProject() {
         for(int sectionIndex=0; sectionIndex<project.getSectionIDs().size(); sectionIndex++){
             ViewGroup sectionView = (ViewGroup) layoutGroups.getChildAt(sectionIndex);
             ViewGroup sectionViewSub = (ViewGroup) sectionView.getChildAt(0);
             FlowLayout sectionViewSubContainer = (FlowLayout) sectionViewSub.getChildAt(1);
             RealmList<Participant> selectedParticipants = new RealmList<>();
-            for(int participantIndex=0; participantIndex<sectionViewSubContainer.getChildCount(); participantIndex++){
-                View rootContainerView = sectionViewSubContainer.getChildAt(participantIndex);
-                TextView textViewNameParticipant = (TextView) rootContainerView.findViewById(R.id.textViewItemParticipantName);
-                String participantName = (String) textViewNameParticipant.getText();
-                RealmResults<Participant> participant = realm.where(Participant.class)
-                        .equalTo("participantName",participantName)
-                        .findAll();
-                selectedParticipants.add(participant.first());
-            }
+            addParticipantToSectionInProject(sectionViewSubContainer, selectedParticipants);
             project.getSectionIDs().get(sectionIndex).setParticipantIDs(selectedParticipants);
             project.getSectionIDs().get(sectionIndex).setSectionID(sectionIndex+1);
         }
-        realm.beginTransaction();
-        realm.copyToRealm(project);
-        realm.commitTransaction();
-        Intent intent = new Intent(view.getContext(), CreateProjectResultActivity.class);
-        intent.putExtra("projectID",project.getProjectID());
-        startActivity(intent);
+    }
+
+    private void addParticipantToSectionInProject(FlowLayout sectionViewSubContainer, List<Participant> selectedParticipants) {
+        for(int participantIndex=0; participantIndex<sectionViewSubContainer.getChildCount(); participantIndex++){
+            View rootContainerView = sectionViewSubContainer.getChildAt(participantIndex);
+            TextView textViewNameParticipant = (TextView) rootContainerView.findViewById(R.id.textViewItemParticipantName);
+            String participantName = (String) textViewNameParticipant.getText();
+            RealmResults<Participant> participant = realm.where(Participant.class)
+                    .equalTo("participantName",participantName)
+                    .findAll();
+            selectedParticipants.add(participant.first());
+        }
+    }
+
+    private void showDialogNotAssignAllParticipant() {
+        AlertDialog.Builder dialogErrorBuilder = new AlertDialog.Builder(this);
+        dialogErrorBuilder.setMessage("Please assign all participant to any section");
+        dialogErrorBuilder.setCancelable(false);
+        dialogErrorBuilder.setPositiveButton("OK",new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog,int which){
+                    dialog.dismiss();
+                }
+        });
+        dialogErrorBuilder.show();
     }
 
     private long generateProjectID(Realm realm) {
